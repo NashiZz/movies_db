44d9bf3b554810ebe07661b71f2fffee4bcf495a
Nashi <115577029+NashiZz@users.noreply.github.com>
2024-12-16 22:41:42 +0700

    update_loaddata

--- src/main/java/digio/co/th/movies_db/Dto/TMDBRes.java
+++ src/main/java/digio/co/th/movies_db/Dto/TMDBRes.java
@@ -6,5 +6,15 @@ import java.util.List;
 
 @Data
 public class TMDBRes {
+    private Dates dates;
+    private Integer page;
     private List<TMDB_Movie> results;
+    private Integer total_pages;
+    private Integer total_results;
+
+    @Data
+    public static class Dates {
+        private String maximum;
+        private String minimum;
+    }
 }
--- src/main/java/digio/co/th/movies_db/Service/MovieDataLoader.java
+++ src/main/java/digio/co/th/movies_db/Service/MovieDataLoader.java
@@ -27,7 +27,7 @@ public class MovieDataLoader implements CommandLineRunner {
     @Autowired
     private GenresRepo genresRepo;
 
-    private final String TMDB_MOVIE_API_URL = "https://api.themoviedb.org/3/trending/all/day";
+    private final String TMDB_MOVIE_API_URL = "https://api.themoviedb.org/3/movie/now_playing";
     private final String TMDB_GENRE_API_URL = "https://api.themoviedb.org/3/genre/movie/list";
     private final String API_KEY = "8d412ae1959630fbb306cdb1b45b0979";
 
@@ -60,44 +60,62 @@ public class MovieDataLoader implements CommandLineRunner {
     }
 
     private void loadMovies() {
-        String url = String.format("%s?api_key=%s&language=en-US", TMDB_MOVIE_API_URL, API_KEY);
-        RestTemplate restTemplate = new RestTemplate();
-        TMDBRes movieResponse = restTemplate.getForObject(url, TMDBRes.class);
+        int page = 1;
+        int totalPages = 1;
+        int maxPages = 5;
 
-        if (movieResponse != null && movieResponse.getResults() != null) {
-            for (TMDB_Movie tmdbMovie : movieResponse.getResults()) {
-                Optional<Movies> existingMovie = movieRepo.findById(tmdbMovie.getId());
+        while (page <= totalPages && page <= maxPages) {
+            try {
+                String url = String.format("%s?api_key=%s&language=en-US&page=%d", TMDB_MOVIE_API_URL, API_KEY, page);
+                RestTemplate restTemplate = new RestTemplate();
 
-                Movies movie = existingMovie.orElseGet(Movies::new);
-                movie.setIdmovie(tmdbMovie.getId());
+                TMDBRes movieResponse = restTemplate.getForObject(url, TMDBRes.class);
 
-                if (tmdbMovie.getTitle() != null && !tmdbMovie.getTitle().isEmpty()) {
-                    movie.setTitle(tmdbMovie.getTitle());
-                } else if (tmdbMovie.getName() != null && !tmdbMovie.getName().isEmpty()) {
-                    movie.setTitle(tmdbMovie.getName());
+                if (movieResponse != null && movieResponse.getResults() != null) {
+                    totalPages = movieResponse.getTotal_pages();
+
+                    for (TMDB_Movie tmdbMovie : movieResponse.getResults()) {
+                        Optional<Movies> existingMovie = movieRepo.findById(tmdbMovie.getId());
+
+                        Movies movie = existingMovie.orElseGet(Movies::new);
+                        movie.setIdmovie(tmdbMovie.getId());
+
+                        if (tmdbMovie.getTitle() != null && !tmdbMovie.getTitle().isEmpty()) {
+                            movie.setTitle(tmdbMovie.getTitle());
+                        } else if (tmdbMovie.getName() != null && !tmdbMovie.getName().isEmpty()) {
+                            movie.setTitle(tmdbMovie.getName());
+                        }
+
+                        movie.setOverview(tmdbMovie.getOverview());
+
+                        if (tmdbMovie.getRelease_date() != null && !tmdbMovie.getRelease_date().isEmpty()) {
+                            movie.setRelease_date(tmdbMovie.getRelease_date());
+                        } else if (tmdbMovie.getFirst_air_date() != null && !tmdbMovie.getFirst_air_date().isEmpty()) {
+                            movie.setRelease_date(tmdbMovie.getFirst_air_date());
+                        }
+
+                        movie.setRating(tmdbMovie.getVote_average());
+                        movie.setPoster_path(tmdbMovie.getPoster_path());
+                        movie.setBackground_path(tmdbMovie.getBackdrop_path());
+
+                        Set<Genres> movieGenres = new HashSet<>(genresRepo.findAllById(tmdbMovie.getGenre_ids()));
+
+                        if (!movieGenres.isEmpty()) {
+                            movie.setGenres(movieGenres);
+                            movieRepo.save(movie);
+                        } else {
+                            System.out.println("No genres found for movie with ID: " + tmdbMovie.getId());
+                        }
+                    }
                 }
 
-                movie.setOverview(tmdbMovie.getOverview());
+                page++;
+                Thread.sleep(500);
 
-                if (tmdbMovie.getRelease_date() != null && !tmdbMovie.getRelease_date().isEmpty()) {
-                    movie.setRelease_date(tmdbMovie.getRelease_date());
-                } else if (tmdbMovie.getFirst_air_date() != null && !tmdbMovie.getFirst_air_date().isEmpty()) {
-                    movie.setRelease_date(tmdbMovie.getFirst_air_date());
-                }
-
-                movie.setRating(tmdbMovie.getVote_average());
-                movie.setPoster_path(tmdbMovie.getPoster_path());
-                movie.setBackground_path(tmdbMovie.getBackdrop_path());
-
-                Set<Genres> movieGenres = new HashSet<>(genresRepo.findAllById(tmdbMovie.getGenre_ids()));
-
-                if (!movieGenres.isEmpty()) {
-                    movie.setGenres(movieGenres);
-
-                    movieRepo.save(movie);
-                } else {
-                    System.out.println("No genres found for movie with ID: " + tmdbMovie.getId());
-                }
+            } catch (Exception e) {
+                System.err.println("Error occurred while loading movies for page: " + page);
+                e.printStackTrace();
+                break;
             }
         }
     }
